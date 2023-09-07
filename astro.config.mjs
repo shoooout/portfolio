641f5e07ecfe7b4c026815f079b09654ac7ae580
shoooout <xabdjx@gmail.com>
2023-09-07 23:11:52 +0900

    不要なファイルを削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  buildOptions: {
-    site: "https://shoooout.github.io/",
-  },
+  site: "https://shoooout.github.io/"
 });
